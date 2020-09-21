392b0ae560d46ffd23162c9fe5091c6d988922a4
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-21 20:04:48 +0530

    Add project files.

---
+++ NUnitTestProject1/UserRegistrationTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    public class UserRegistrationTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
